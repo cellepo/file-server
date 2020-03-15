34064a8418801a9f11985a7e0dfe00c5f4c2bf82
Chris Ellepola <cellepo@gmail.com>
2020-03-15 10:39:31 -0700

    POC streaming (https://www.logicbig.com/tutorials/spring-framework/spring-web-mvc/streaming-response-body.html)

--- src/main/java/com/chrisellepola/fileserver/Controller.java
+++ src/main/java/com/chrisellepola/fileserver/Controller.java
@@ -2,8 +2,14 @@ package com.chrisellepola.fileserver;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @RestController
 public class Controller {
@@ -12,6 +18,30 @@ public class Controller {
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void healthCheck(){
 
+    }
+
+    @GetMapping("/store")
+    public StreamingResponseBody read() throws IOException {
+        return new StreamingResponseBody() {
+            @Override
+            public void writeTo(OutputStream out) throws IOException {
+                for (int i = 0; i < 1000; i++) {
+                    out.write((i + " - ").getBytes());
+                    out.flush();
+                    try {
+                        Thread.sleep(5);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+    }
+
+    @PostMapping("/store")
+    public void create() throws IOException {
+        (new File("test.txt")).createNewFile();
     }
 
 }
