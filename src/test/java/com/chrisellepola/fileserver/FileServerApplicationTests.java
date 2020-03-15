3414d0099144c7275e4976eecd71fa85fc2f6a40
Chris Ellepola <cellepo@gmail.com>
2020-03-14 19:25:06 -0700

    Initialize Spring Boot project (https://spring.io/guides/gs/rest-service)

---
+++ src/test/java/com/chrisellepola/fileserver/FileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisellepola.fileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
